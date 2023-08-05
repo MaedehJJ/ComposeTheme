f26802ca724849c19f9ac71690710ad9f7fd60e7
Maedeh Jalali <majj2000.mail@gmail.com>
2023-08-05 21:02:32 +0330

    Convert a text view in xml to a Compose version

---
+++ MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantName.kt
@@ -0,0 +1,36 @@
+package com.google.samples.apps.sunflower.plantdetail
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.google.samples.apps.sunflower.R
+
+@Composable
+fun PlantName(
+    modifier: Modifier = Modifier,
+    title: String
+) {
+    Text(
+        text = title,
+        modifier = modifier
+            .padding(horizontal = dimensionResource(id = R.dimen.margin_small))
+            .fillMaxWidth()
+            .wrapContentWidth(Alignment.CenterHorizontally),
+        style = MaterialTheme.typography.h5
+    )
+}
+
+@Preview
+@Composable
+private fun PlantNamePreview() {
+    MaterialTheme {
+        PlantName(title = "Jasmin")
+    }
+}
